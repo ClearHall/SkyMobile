831cbb4162a48cc6d97c054b4ba92d71e4984ded
lingfeishengtian <602353hunter>
2020-04-01 11:20:01 -0500

    Support for web demo!

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,14 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+void registerPlugins(PluginRegistry registry) {
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
+}
